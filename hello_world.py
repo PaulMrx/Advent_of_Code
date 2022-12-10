5839c531fe890e11a8b6315747a3f543914f7136
lbreede <lennart.breede@gmail.com>
2022-12-09 17:48:47 -0800

    Turned if statement into single line

--- hello_world.py
+++ hello_world.py
@@ -7,13 +7,10 @@ final = []
 for count, letter in enumerate(string):
     list1 += [string[0+count:14+count]]
 
-#print(list1)
+# print(list1)
 for i in list1:
     test = set(list(i))
-    if len(test) is not 14:
-        res = 1
-    else:
-        res = 0
+    res = 1 if len(test) is not 14 else 0
     final.append(res)
 print(final)
 #
