fa07bc5dba744ee85ac4a42184a0b32217698f33
PaulMrx <120225682+PaulMrx@users.noreply.github.com>
2022-12-10 01:25:45 +0000

    testing new script

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+import string
+
+string = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+
+list1 = []
+final = []
+for count, letter in enumerate(string):
+    list1 += [string[0+count:14+count]]
+
+#print(list1)
+for i in list1:
+    test = set(list(i))
+    if len(test) is not 14:
+        res = 1
+    else:
+        res = 0
+    final.append(res)
+print(final)
+#
+print(final.index(0)+14)
