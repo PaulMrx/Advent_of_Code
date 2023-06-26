a3d31510e0524df813e63a82f7b825a69e42f8a8
PaulMrx <120225682+PaulMrx@users.noreply.github.com>
2023-06-26 10:49:23 -0700

    Update 2022_Day_01.py
    copied from Houdini version

--- 2022_Day_01.py
+++ 2022_Day_01.py
@@ -1,1 +1,17 @@
+import hou
 
+string = input("Enter puzzle input")
+
+bucket = string.split("\n\n")
+list = []
+
+for n in bucket:
+    food = n.split("\n")
+    res = [eval(i) for i in food]
+    list.append(sum(res))
+    
+    maxCalories = max(list)
+    lastOnes = sum(sorted(list)[-3:])
+
+print("result": maxCalories)
+print("lastOnes": lastOnes)
