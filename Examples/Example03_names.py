5fb071e3754c8ec3edf701e25d03924ccd705adf
PaulMrx <120225682+PaulMrx@users.noreply.github.com>
2023-06-30 00:57:03 +0000

    new example

---
+++ Examples/Example03_names.py
@@ -0,0 +1,32 @@
+
+
+# # Open file with "append" function, write the input name, close the file
+# file = open("Example03_names.json", "a")
+# file.write(f"{name} \n")
+# file.close()
+
+# Same but in a better / shorter way
+# with open("Example03_names.json", "a") as file:
+#     file.write(f"{name} \n")
+
+
+
+# Now proper test
+
+def write_names():
+    with open("Examples/Example03_names.json", "a") as file:
+        file.write(f"{name} \n")
+
+def read_names():
+    with open("Examples/Example03_names.json", "r") as files:
+        for file in files:
+            print(f"My name is {file} \n")
+
+
+while True:
+    name = input("What's your name?").strip().title()
+    if name != "Done":
+        write_names()
+    else:
+        read_names()
+        break
